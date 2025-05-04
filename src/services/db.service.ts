2192b973384704c9a8b0b205e95151065975aee1
abogach <ata.hosgul@windowslive.com>
2025-05-04 19:30:08 +0200

    Improving DB timeout and argument issues

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -3,18 +3,20 @@ import path from 'path';
 
 const db = new sqlite(path.resolve('./db/db.sqlite3'), {
 	fileMustExist: true,
+	// 5 seconds timeout for database operations to avoid deadlocks
+	timeout: 5000,
 });
 
 function query(
 	sql: string,
-	params?: { [key: string]: string | number | undefined },
+	params?: any[],
 ) {
 	return params ? db.prepare(sql).all(params) : db.prepare(sql).all();
 }
 
 function run(
 	sql: string,
-	params?: { [key: string]: string | number | undefined },
+	params?: any[],
 ) {
 	return params ? db.prepare(sql).run(params) : db.prepare(sql).run();
 }
